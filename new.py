601ff33b12812909f5839ef2c9267a4d5ba7b46c
Victor Morris <92633344+Victorrmorris@users.noreply.github.com>
2023-04-03 17:10:21 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,10 +1,36 @@
 import streamlit as st
 
-st.title("Analytic Inquiry")
+def main():
+    st.title("Transferable Skills and Applications Form")
 
-topic = st.selectbox("Select a topic", ["Identify and frame a problem or question", "Distinguish among ideas concepts, theories or practical approaches to the problem"])
+    # Intellectual Skills Section
+    st.header("Intellectual Skills")
+    st.write("Please select your top transferable intellectual skills:")
+    intellectual_skills_options = ["Analytic Inquiry", "Use of Information and Resources", 
+                                  "Engaging Diverse Perspectives", "Ethical Reasoning", 
+                                  "Quantitative Fluency", "Communication Fluency"]
+    intellectual_skills = st.selectbox("Intellectual Skills", intellectual_skills_options)
 
-if topic == "Identify and frame a problem or question":
-    st.write("You have selected 'Identify and frame a problem or question' topic.")
-elif topic == "Distinguish among ideas concepts, theories or practical approaches to the problem":
-    st.write("You have selected 'Distinguish among ideas concepts, theories or practical approaches to the problem' topic.")
+    # Applied and Collaborative Learning Section
+    st.header("Applied and Collaborative Learning")
+    st.write("Please describe any practical applications or examples of how you have used your intellectual skills in a collaborative or applied setting:")
+    applied_learning = st.text_area("Applied and Collaborative Learning")
+
+    # Civic and Global Learning Section
+    st.header("Civic and Global Learning")
+    st.write("Please describe any experiences or examples of how you have applied your intellectual skills to civic or global issues:")
+    civic_learning = st.text_area("Civic and Global Learning")
+
+    # Submit Button
+    if st.button("Submit"):
+        # Store the responses in a dictionary
+        responses = {
+            "intellectual_skills": intellectual_skills,
+            "applied_learning": applied_learning,
+            "civic_learning": civic_learning
+        }
+        # Print the responses to the console
+        print(responses)
+
+if __name__ == "__main__":
+    main()
